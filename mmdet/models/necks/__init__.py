4136bdde29f39b2fbf23f9b0f1e335ad06314e31
Xuyihaoby <1219427816@qq.com>
2021-08-30 15:32:30 +0800

    add ornfpn (not test)

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -10,9 +10,11 @@ from .rfp import RFP
 from .yolo_neck import YOLOV3Neck
 
 from .dcn_lat_fpn import DcnLatFPN
+from .orfpn import ORFPN
 
 __all__ = [
     'FPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
     'NASFCOS_FPN', 'RFP', 'YOLOV3Neck',
-    'DcnLatFPN'
+    'DcnLatFPN',
+    'ORFPN'
 ]
