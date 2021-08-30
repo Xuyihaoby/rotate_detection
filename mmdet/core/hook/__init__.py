3b12404d1bce0b3c85334581787fc471c319130d
Xuyihaoby <1219427816@qq.com>
2021-08-30 14:01:06 +0800

    add ema hook

--- mmdet/core/hook/__init__.py
+++ mmdet/core/hook/__init__.py
@@ -1,5 +1,7 @@
 from .mode_switch_hook import ModeSwitchHook
+from .ema import ExpMomentumEMAHook, LinearMomentumEMAHook
 
 __all__ = [
-    'ModeSwitchHook'
+    'ModeSwitchHook',
+    'ExpMomentumEMAHook', 'LinearMomentumEMAHook'
 ]
