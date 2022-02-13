f931a6fbfb824168f389c4148828474508d81694
Xuyihaoby <1219427816@qq.com>
2022-02-13 22:19:06 +0800

    update heatmap

--- mmdet/utils/heatmap.py
+++ mmdet/utils/heatmap.py
@@ -8,18 +8,22 @@ import time
 import torch.nn.functional as F
 
 
-def showHeatmap(feat, path='/home/xyh/rotate_detection/checkpoints/visual/heat/', img_metas=None):
+def showHeatmap(feat, path='/home/xyh/rotate_detection/checkpoints/visual/heat/', img_metas=None, rescale=True):
     os.makedirs(path, exist_ok=True)
     if isinstance(feat, list):
         # size = [_feat.shape[-2] for _feat in feat]
         new_feat_list = [F.interpolate(_feat, size=256, mode='bilinear') for _feat in feat]
         new_feat = torch.cat(new_feat_list)
         new_feat, _ = new_feat.sum(1).max(0)
-
         new_feat = new_feat.detach().cpu().numpy()
+        # flatten_feat = new_feat.flatten()
+        max_item = new_feat.max()
         norm_img = None
         norm_img = cv.normalize(new_feat, norm_img, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8U)
+        if rescale:
+            norm_img = (norm_img * max_item / 255).astype(dtype=np.uint8)
         heat_img = cv.applyColorMap(norm_img, cv.COLORMAP_JET)  # 注意此处的三通道热力图是cv2专有的GBR排列
+        # heat_img = heat_img * max_item / 255
         resize_heatimg = cv.resize(heat_img, (512, 512), interpolation=cv.INTER_AREA)
 
         ori_img = cv.imread(img_metas[0]['filename'])
