deb97a052f943b25c2bcf278f621186d0587581f
Xuyihaoby <1219427816@qq.com>
2022-11-09 19:33:18 +0800

    update hornet readme

---
+++ configs/hornet/README.md
@@ -0,0 +1,18 @@
+# HorNet: Efficient High-Order Spatial Interactions with Recursive Gated Convolutions
+
+[HorNet: Efficient High-Order Spatial Interactions with Recursive Gated Convolutions](http://arxiv.org/abs/2207.14284)
+
+## abstract
+
+Recent progress in vision Transformers exhibits great success in various tasks driven by the new spatial modeling mechanism based on dot-product self-attention. In this paper, we show that the key ingredients behind the vision Transformers, namely input-adaptive, long-range and high-order spatial interactions, can also be efficiently implemented with a convolution-based framework. We present the Recursive Gated Convolution ($\textit{g}^\textit{n}$Conv) that performs high-order spatial interactions with gated convolutions and recursive designs. The new operation is highly flexible and customizable, which is compatible with various variants of convolution and extends the two-order interactions in self-attention to arbitrary orders without introducing significant extra computation. $\textit{g}^\textit{n}$Conv can serve as a plug-and-play module to improve various vision Transformers and convolution-based models. Based on the operation, we construct a new family of generic vision backbones named HorNet. Extensive experiments on ImageNet classification, COCO object detection and ADE20K semantic segmentation show HorNet outperform Swin Transformers and ConvNeXt by a significant margin with similar overall architecture and training configurations. HorNet also shows favorable scalability to more training data and a larger model size. Apart from the effectiveness in visual encoders, we also show $\textit{g}^\textit{n}$Conv can be applied to task-specific decoders and consistently improve dense prediction performance with less computation. Our results demonstrate that $\textit{g}^\textit{n}$Conv can be a new basic module for visual modeling that effectively combines the merits of both vision Transformers and CNNs. Code is available at https://github.com/raoyongming/HorNet
+
+## Results and Models
+
+| Method | Backbone            | Angle  | Loss  | Lr schd | Dataset         | preprocess    | $AP_{0.5}$ | $AP_{0.75}$ | $mAP$ |
+| ------ | ------------------- | ------ | ----- | ------- | --------------- | ------------- | ---------- | ----------- | ----- |
+| retina | hornet_tiny         | v1(oc) | R_IoU | 1x      | DOTA-v1.0,train | 1024x1024,512 | 70.40      | 40.92       | 40.74 |
+| retina | hornet_tiny_gnfpn   | v1(oc) | R_IoU | 1x      | DOTA-v1.0,train | 1024x1024,512 | 67.81      | 36.66       | 38.18 |
+| retina | hornet_tiny_with_gf | v1(oc) | R_IoU | 1x      | DOTA-v1.0,train | 1024x1024,512 | 70.02      | 39.24       | 40.03 |
+
+**note**:If directly regress five parameters and use R_IoU loss, the model can not converge well!!
+
---
+++ configs/hornet/retinanet_hornet_tiny_gf_fpn_iou_1x.py
@@ -0,0 +1,162 @@
+model = dict(
+    type='RRetinaNet',
+    pretrained='./checkpoints/hornet_tiny_gf.pth',
+    backbone=dict(
+        type='HorNet',
+        depths=[2, 3, 18, 2],
+        base_dim=64,
+        block='Block',
+        gnconv=[
+            'partial(gnconv, order=2, s=1/3)',
+            'partial(gnconv, order=3, s=1/3)',
+            'partial(gnconv, order=4, s=1/3, h=14, w=8, gflayer=GlobalLocalFilter)',
+            'partial(gnconv, order=5, s=1/3, h=7, w=4, gflayer=GlobalLocalFilter)',
+        ],
+        drop_path_rate=0.4,
+        out_indices=[0, 1, 2, 3],
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 256, 512],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5),
+    bbox_head=dict(
+        type='RRetinaHead',
+        num_classes=15,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        reg_decoded_bbox=True,
+        anchor_generator=dict(
+            type='RAnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[8, 16, 32, 64, 128],
+            angles=[0.]),
+        bbox_coder=dict(
+            type='DeltaRXYWHThetaBBoxCoder',
+            target_means=[.0, .0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='RotatedIoULoss', loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1,
+            iou_calculator=dict(type='RBboxOverlaps2D')),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='rnms', iou_threshold=0.1),
+        max_per_img=2000))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+total_epochs = 12
+
+dataset_type = 'DOTADatasetV1'
+data_root = '/data1/public_dataset/DOTA1_0/simple/'
+trainsplit_ann_folder = 'train/labelTxt'
+trainsplit_img_folder = 'train/images'
+valsplit_ann_folder = 'train/labelTxt'
+valsplit_img_folder = 'train/images'
+val_ann_folder = 'train/labelTxt'
+val_img_folder = 'train/images'
+test_img_folder = 'test/images'
+example_ann_folder = 'train/labelTxt'
+example_img_folder = 'train/images'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='RLoadAnnotations', with_bbox=True, with_mask=False),
+    dict(type='RResize', img_scale=(1024, 1024)),
+    dict(type='RRandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_bboxes_ignore', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(type='RRandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + trainsplit_ann_folder,
+        img_prefix=data_root + trainsplit_img_folder,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + valsplit_ann_folder,
+        img_prefix=data_root + valsplit_img_folder,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + test_img_folder,
+        img_prefix=data_root + test_img_folder,
+        pipeline=test_pipeline,
+        test_mode=True))
+evaluation = dict(interval=24, metric='bbox')
+
+checkpoint_config = dict(interval=6)
+
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+work_dir = '/data1/xyh/checkpoints/hornet/rotate_iou_gf'
+# mAP: 0.7002472330537783
+# ap of each class: plane:0.8931362894461936, baseball-diamond:0.7357304555787438, bridge:0.4090411027020368, ground-track-field:0.7064931611547968, small-vehicle:0.7806933449744234, large-vehicle:0.655796603737089, ship:0.8375297039548575, tennis-court:0.9081296006712691, basketball-court:0.8038711826687914, storage-tank:0.8061964161657965, soccer-ball-field:0.5638550094381927, roundabout:0.6286416551929765, harbor:0.5197517171687988, swimming-pool:0.6692147205613512, helicopter:0.5856275323913589
+# COCO style result:
+#
+# AP50: 0.7002472330537783
+# AP75: 0.3924342673996018
+# mAP: 0.4002672797814343
