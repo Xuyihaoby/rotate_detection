1b04b28c36d9b8a60c297cc7daffc4c261593362
Xuyihaoby <1219427816@qq.com>
2023-01-06 15:48:31 +0800

    refactor configs

--- configs/rcascade_rcnn/cascade_rcnn_r50_fpn_1x.py
+++ configs/rcascade_rcnn/cascade_rcnn_r50_fpn_1x.py
@@ -47,7 +47,7 @@ model = dict(
                 in_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=11,
+                num_classes=15,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
                     target_means=[0., 0., 0., 0.],
@@ -68,7 +68,7 @@ model = dict(
                 in_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=11,
+                num_classes=15,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
                     target_means=[0., 0., 0., 0.],
@@ -89,7 +89,7 @@ model = dict(
                 in_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=11,
+                num_classes=15,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
                     target_means=[0., 0., 0., 0.],
@@ -113,7 +113,8 @@ model = dict(
                 neg_iou_thr=0.3,
                 min_pos_iou=0.3,
                 match_low_quality=True,
-                ignore_iof_thr=-1),
+                ignore_iof_thr=-1,
+                gpu_assign_thr=180),
             sampler=dict(
                 type='RandomSampler',
                 num=256,
@@ -190,12 +191,15 @@ model = dict(
             min_bbox_size=0),
         rcnn=dict(
             score_thr=0.05,
-            nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100))
+            nms_h=dict(type='nms', iou_threshold=0.1),
+            max_per_img_h=500,
+            nms_r=dict(type='rnms', iou_threshold=0.1),
+            # nms_r=dict(type='nms_rotate', iou_threshold=merge_nms_iou_thr_dict),
+            max_per_img=500))
 )
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -206,23 +210,23 @@ lr_config = dict(
     step=[8, 11])
 total_epochs = 12
 
-dataset_type = 'RSAI'
-data_root = '/data1/public_dataset/rsai/split/'
-trainsplit_ann_folder = 'labelTxt'
-trainsplit_img_folder = 'images'
-valsplit_ann_folder = 'labelTxt'
-valsplit_img_folder = 'images'
-val_ann_folder = 'labelTxt'
-val_img_folder = 'images'
-test_img_folder = 'images'
-example_ann_folder = 'labelTxt'
-example_img_folder = 'images'
+dataset_type = 'DOTADatasetV1'
+data_root = '/data1/public_dataset/DOTA/DOTA1_0/simple/'
+trainsplit_ann_folder = 'train/labelTxt'
+trainsplit_img_folder = 'train/images'
+valsplit_ann_folder = 'train/labelTxt'
+valsplit_img_folder = 'train/images'
+val_ann_folder = 'train/labelTxt'
+val_img_folder = 'train/images'
+test_img_folder = 'test/images'
+example_ann_folder = 'train/labelTxt'
+example_img_folder = 'train/images'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RLoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='RLoadAnnotations', with_bbox=True),
     dict(type='RResize', img_scale=(1024, 1024)),
     dict(type='RRandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
@@ -248,7 +252,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=4,
-    workers_per_gpu=0,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + trainsplit_ann_folder,
@@ -281,4 +285,11 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-work_dir = '/home/lzy/xyh/Netmodel/rotate_detection/checkpoints/rsai/cascade_rcnn_r50'
+work_dir = '/home/lzy/xyh/Netmodel/rotate_detection/checkpoints/simDOTA1_0/cascade_rcnn_r50'
+
+# mAP: 0.6967120871609882
+# ap of each class: plane:0.8125759775905526, baseball-diamond:0.7551112683672065, bridge:0.47292842181621825,
+# ground-track-field:0.6963827330520076, small-vehicle:0.7333475786703573, large-vehicle:0.7656121439032034,
+# ship:0.7841171973733069, tennis-court:0.9089550210626446, basketball-court:0.7936236847382849,
+# storage-tank:0.7799624393970619, soccer-ball-field:0.5830999736135294, roundabout:0.5976809432669088,
+# harbor:0.6731636112766847, swimming-pool:0.6267668226775515, helicopter:0.46735349060930453
